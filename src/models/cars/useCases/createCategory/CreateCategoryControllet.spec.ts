c99695ed9e0807e2a6e8649434c660ce98e0d4e3
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2021-04-27 12:02:52 -0300

    test database connection

--- src/models/cars/useCases/createCategory/CreateCategoryControllet.spec.ts
+++ src/models/cars/useCases/createCategory/CreateCategoryControllet.spec.ts
@@ -2,7 +2,12 @@ import request from 'supertest';
 import app from '@shared/infra/http/app';
 
 describe('CreateCategory Controller', () => {
-    it('TEST', async () => {
-        await request(app).get('/carts/available').expect(200);
+    it('should be able to create a new category', async () => {
+        const response = await request(app).post('/categories').send({
+            name: 'Example Name',
+            description: 'Example Description',
+        });
+
+        expect(response.status).toEqual(201);
     });
 });
