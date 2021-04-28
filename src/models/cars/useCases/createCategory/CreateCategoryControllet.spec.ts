36e7ba214e0cc240d551e8e7238553c892fd700a
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2021-04-28 08:13:13 -0300

    create category integration test

--- src/models/cars/useCases/createCategory/CreateCategoryControllet.spec.ts
+++ src/models/cars/useCases/createCategory/CreateCategoryControllet.spec.ts
@@ -1,13 +1,50 @@
 import request from 'supertest';
+import { Connection } from 'typeorm';
+import { hash } from 'bcrypt';
+import { v4 as uuidv4 } from 'uuid';
+
 import app from '@shared/infra/http/app';
+import createConnection from '@shared/infra/typeorm';
+
+let connection: Connection;
 
 describe('CreateCategory Controller', () => {
+    beforeAll(async () => {
+        connection = await createConnection();
+        await connection.runMigrations();
+
+        const id = uuidv4();
+        const password = await hash('admin', 8);
+        await connection.query(`
+            INSERT INTO USERS(id, name, email, password, is_admin, created_at, driver_license)
+            VALUES ('${id}', 'admin', 'admin@rentx.com.br', '${password}', true, 'now()', 'AAA-ZZZZ')
+        `);
+    });
+
+    afterAll(async () => {
+        await connection.dropDatabase();
+        await connection.close();
+    });
+
     it('should be able to create a new category', async () => {
-        const response = await request(app).post('/categories').send({
-            name: 'Example Name',
-            description: 'Example Description',
+        const responseToken = await request(app).post('/sessions').send({
+            email: 'admin@rentx.com.br',
+            password: 'admin',
         });
 
+        const { token } = responseToken.body;
+
+        const response = await request(app)
+            .post('/categories')
+            .send({
+                name: 'Example Name',
+                description: 'Example Description',
+            })
+            .set({
+                Authorization: `Bearer ${token}`,
+            });
+
         expect(response.status).toEqual(201);
+        expect(response.body).toHaveProperty('id');
     });
 });
