3c55163859722acfcbd7a1e0596734be104ec42e
Vinícius Gabriel <68403363+Vinicius7Dev@users.noreply.github.com>
2021-06-18 07:48:03 -0300

    add server port as enviroment var

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(3333, () => {
-    console.log('===> Server started on port 3333 <===');
+const serverPort = process.env.SERVER_PORT || 3333;
+
+app.listen(serverPort, () => {
+    console.log(`===> Server started on port ${serverPort} <===`);
 });
