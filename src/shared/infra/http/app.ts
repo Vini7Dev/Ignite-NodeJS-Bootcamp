0b68c3d46934838db75b0273b62e2065c472be16
Vinícius Gabriel <viniciusgabriel.dev@gmail.com>
2021-06-21 08:49:30 -0300

    add sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,8 @@ import 'dotenv/config';
 import 'express-async-errors';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 import swaggerUi from 'swagger-ui-express';
 import AppError from '@shared/errors/AppError';
 import upload from '@config/upload';
@@ -17,11 +19,24 @@ createConnection();
 
 const app = express();
 
+app.use(rateLimiter);
+
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use(cors());
-
-app.use(rateLimiter);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
@@ -30,9 +45,7 @@ app.use('/car', express.static(`${upload.tempFolder}/car`));
 
 app.use(mainRoutes);
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'OK' });
-});
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
     (
